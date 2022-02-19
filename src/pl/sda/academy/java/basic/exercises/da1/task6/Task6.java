7479c57e8ef3100569c7ca655e43c258a9694542
Dominik <dkieltyka95@gmial.com>
2022-02-19 14:55:57 +0100

    task6

---
+++ src/pl/sda/academy/java/basic/exercises/da1/task6/Task6.java
@@ -0,0 +1,23 @@
+package pl.sda.academy.java.basic.exercises.da1.task6;
+
+public class Task6 {
+
+    public static void main(String[] args) {
+        printMultiplicationTable(6, 2, 5);
+
+    }
+
+    private static void printMultiplicationTable(int multiplier, int minMultiplicand, int maxMultiplicand) {
+        for (; minMultiplicand <= maxMultiplicand; minMultiplicand++) {
+            int result = multiplier * minMultiplicand;
+            System.out.println(multiplier + " * " + minMultiplicand + " = " + result);
+        }
+
+            while (minMultiplicand <= maxMultiplicand) {
+                int result = multiplier * minMultiplicand;
+                System.out.println(multiplier + " * " + minMultiplicand + " = " + result);
+                minMultiplicand++;
+            }
+        }
+    }
+
